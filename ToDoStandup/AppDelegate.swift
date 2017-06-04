9db0c9a514eb2f9b3262a8dc597dc299107e3726
Peter Zimbelman <pzimbelman@gmail.com>
2017-06-04 17:44:29 -0400

    Remove storyboards and get app running without them

--- ToDoStandup/AppDelegate.swift
+++ ToDoStandup/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = UIViewController()
+        homeViewController.view.backgroundColor = UIColor.red
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
